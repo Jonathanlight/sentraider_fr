0f02476d6b0d34ad97700859b6a208112777a8f0
jonathan <jonathan.kablan@sooyoos.com>
2020-04-13 05:15:15 +0200

    install api-plateform

--- app/symfony/config/bundles.php
+++ app/symfony/config/bundles.php
@@ -12,4 +12,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
