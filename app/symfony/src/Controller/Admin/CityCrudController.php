1764dcf8df43de07b7a68a4547c998f211f3c8e9
Jonathan <jonathan.kablan@gmail.com>
2020-07-26 03:59:38 +0200

    custom field

--- app/symfony/src/Controller/Admin/CityCrudController.php
+++ app/symfony/src/Controller/Admin/CityCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\City;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
@@ -16,16 +17,42 @@ class CityCrudController extends AbstractCrudController
         return City::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPageTitle(Crud::PAGE_INDEX, 'Liste des villes')
+            ->overrideTemplate('crud/edit', 'bundles/EasyAdminBundle/custom/crud_edit_custom.html.twig')
+            ->overrideTemplate('crud/index', 'bundles/EasyAdminBundle/custom/crud_index_custom.html.twig')
+            ->overrideTemplate('crud/detail', 'bundles/EasyAdminBundle/custom/crud_detail_custom.html.twig')
+            ->overrideTemplate('crud/new', 'bundles/EasyAdminBundle/custom/crud_new_custom.html.twig')
+        ;
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
+        $imageField = ImageField::new('imageFile')
+            ->setFormType(VichImageType::class)
+            ->setLabel('Image');
+
+        $image = ImageField::new('image')
+            ->setBasePath("/uploads/images")
+            ->setLabel('Image');
+
+        $fields = [
             IntegerField::new('id', 'ID')->onlyOnIndex(),
-            TextField::new('slug'),
-            TextField::new('name'),
-            ImageField::new('imageFile')
-                ->setFormType(VichImageType::class)
-                ->setLabel('Image'),
+            TextField::new('slug')
+                ->setTemplatePath('bundles/EasyAdminBundle/field_custom.html.twig'),
+            TextField::new('name')
+                ->setTemplatePath('bundles/EasyAdminBundle/field_custom.html.twig'),
         ];
+
+        if ($pageName === Crud::PAGE_INDEX || $pageName === Crud::PAGE_DETAIL) {
+            $fields[] = $image;
+        } else {
+            $fields[] = $imageField;
+        }
+
+        return $fields;
     }
 
 }
